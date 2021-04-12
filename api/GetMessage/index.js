fb7f22ab842b50d34e9dee714ab52d5a77c63b1c
Brian McKeiver <mcbeev@gmail.com>
2021-04-11 20:20:19 -0400

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,13 +1,9 @@
+const fetch = require("node-fetch");
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    const url = 'https://app.symreport.com/rest/customtableitem.sc.workspace?format=json&columns=Workspacename&hash=1e0c875c6e091e1e62f2e714d4d2f53d4eb193cbbbc522ef62e36df134e95cce';
+    
+    await fetch(url)
+        .then(response => response.json())
+        .then(response => context.res.json(response)); 
 }
